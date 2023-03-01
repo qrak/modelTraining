c4a6f2d8a26f3b2c8ea6c57a5dfa081747522438
qrak <donqrakko@gmail.com>
2023-03-01 14:48:08 +0100

    1.0

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pytorch_lightning as pl
 from classdirectory.classfile import LSTMNet
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.feature_selection import SelectKBest, mutual_info_regression
 from torch.utils.data import DataLoader, TensorDataset
 
 
@@ -25,8 +25,28 @@ if __name__ == '__main__':
     scaler = StandardScaler()
     X = scaler.fit_transform(data.drop(['timestamp', 'close'], axis=1))
     y = scaler.fit_transform(data[['close']]).ravel()
+    # Select the best features
+    selector = SelectKBest(mutual_info_regression, k=10)
+    selector.fit(X, y)
 
-    # Split the data into training and validation sets
+    # Get the selected features and their scores
+    selected_features = data.drop(['timestamp', 'close'], axis=1).columns[selector.get_support()]
+    feature_scores = selector.scores_[selector.get_support()]
+
+    print("Selected features:")
+    for feature, score in zip(selected_features, feature_scores):
+        print(f"{feature}: {score:.4f}")
+
+    # Save the selected features to a file
+    filename = "selected_features.txt"
+    mode = "a" if os.path.exists(filename) else "w"
+    with open(filename, mode) as f:
+        if mode == "w":
+            f.write("Selected features:\n")
+        for feature, score in zip(selected_features, feature_scores):
+            f.write(f"{feature}: {score:.4f}\n")
+    # Use only the selected features for training and validation
+    X = selector.transform(X)
     X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
     X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2, shuffle=False)
 
